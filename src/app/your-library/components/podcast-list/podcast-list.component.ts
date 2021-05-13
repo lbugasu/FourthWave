5897c1f8789681e601fcd88c439be97f3702847c
lbugasu <lbugasu@gmail.com>
2021-05-12 20:40:50 -0400

    updates

---
+++ src/app/your-library/components/podcast-list/podcast-list.component.ts
@@ -0,0 +1,25 @@
+import { AppState } from 'src/app/store/app.state'
+import { Observable } from 'rxjs/Observable'
+import { Component, OnInit } from '@angular/core'
+import { Podcast } from 'src/app/shared/Models'
+import { Store } from '@ngrx/store'
+import { UserSelectors } from 'src/app/user/store'
+
+@Component({
+  selector: 'app-podcast-list',
+  templateUrl: './podcast-list.component.html',
+  styleUrls: ['./podcast-list.component.scss']
+})
+export class PodcastListComponent implements OnInit {
+  podcasts$: Observable<Podcast[]>
+  view: string = 'stacked'
+  constructor (private store: Store<AppState>) {}
+
+  ngOnInit (): void {
+    this.podcasts$ = this.store.select(UserSelectors.getPodcastSubscriptions)
+  }
+
+  changeView (view: string) {
+    this.view = view
+  }
+}
--- src/app/your-library/your-library.module.ts
+++ src/app/your-library/your-library.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { YourLibraryComponent } from './your-library.component'
 import { RouterModule } from '@angular/router'
-import { Material } from 'src/libs'
+import { Material } from 'src/libs';
+import { PodcastListComponent } from './components/podcast-list/podcast-list.component';
+import { EpisodeListComponent } from './components/episode-list/episode-list.component'
 
 const routes = [
   {
@@ -11,7 +13,7 @@ const routes = [
   }
 ]
 @NgModule({
-  declarations: [YourLibraryComponent],
+  declarations: [YourLibraryComponent, PodcastListComponent, EpisodeListComponent],
   imports: [CommonModule, Material, RouterModule.forChild(routes)]
 })
 export class YourLibraryModule {}
