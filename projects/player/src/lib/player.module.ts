511ace6fdf055b55b6553509396e26b57699cbdb
lbugasu <lbugasu@gmail.com>
2022-04-19 16:09:14 -0500

    feat: add player package

---
+++ projects/player/src/lib/player.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core'
+import { PlayerComponent } from './player.component'
+
+@NgModule({
+    declarations: [PlayerComponent],
+    imports: [],
+    exports: [PlayerComponent],
+})
+export class PlayerModule {}
